1217ab5dda4005651b6ef7a951221130af3fce02
lorenzo ridan <joseramosbtc@gmail.com>
2024-05-02 11:01:36 -0300

    commit aula operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,14 +1,40 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
+     /*    int numero = 5;
         numero = - numero;
 
         System.out.println(numero);
 
         numero =  numero * -1;
 
-        System.out.println(numero);
+        System.out.println(numero); */
+      
+        /*
+         boolean variavel = true;
+        // verdadeiro ou falso
 
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+         */
+           
+          // operador ternario
+          
+           int a, b;
+
+           a = 6;
+           b = 6;
+
+           String resultado = a==b ? "verdadeiro" : "falso";
+          /*  if(a==b)
+                resultado = "verdadeiro";
+           else
+                resultado = "falso"; */
+                
+                System.out.println(resultado);
+
+        
          
     }  
     
