84674253a1d97d601e342100e7adc2dbab45e257
lorenzo ridan <joseramosbtc@gmail.com>
2024-05-03 09:29:55 -0300

    operador boolean

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,4 +1,5 @@
 public class Operadores {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
      /*    int numero = 5;
         numero = - numero;
@@ -21,20 +22,27 @@ public class Operadores {
            
           // operador ternario
           
-           int a, b;
+          /*  int a, b;
 
            a = 6;
            b = 6;
 
            String resultado = a==b ? "verdadeiro" : "falso";
-          /*  if(a==b)
+            if(a==b)
                 resultado = "verdadeiro";
            else
-                resultado = "falso"; */
+                resultado = "falso"; 
                 
-                System.out.println(resultado);
+                System.out.println(resultado); */
 
         
+         int numero1 = 1;
+         int numero2 = 2;
+
+         boolean simNao = numero1 == numero2;
+         if(numero1 == numero2){
+            System.out.println("Nossa condicao  verdadeira");
+         }
          
     }  
     
